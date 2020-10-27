948049c350d413392f8a64a25241358362e13b9e
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-10-27 20:48:27 +0530

    Add files via upload

--- Src.py
+++ Src.py
@@ -8,3 +8,30 @@ wn = turtle.Screen()
 wn.bgcolor("blue")
 wn.title("Space Invaders by Mahesh Sawant")
 wn.bgpic("background.gif")
+
+turtle.register_shape("invader.gif")
+turtle.register_shape("player.gif")
+
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+score = 0
+
+
+score_pen = turtle.Turtle()
+score_pen.speed(0)
+score_pen.color("white")
+score_pen.penup()
+score_pen.setposition(-290, 280)
+scorestring = "Score: %s" %score
+score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+score_pen.hideturtle()
